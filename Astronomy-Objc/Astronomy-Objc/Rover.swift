33de2a6f1c3d28e6a19a3a4d3dd236a59d4715d3
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2020-06-03 22:36:57 -0400

    Set up the main.storyboard and added view controller files

--- Astronomy-Objc/Astronomy-Objc/Rover.swift
+++ Astronomy-Objc/Astronomy-Objc/Rover.swift
@@ -1,15 +1,9 @@
 //
-//  ViewController.h
+//  Rover.swift
 //  Astronomy-Objc
 //
 //  Created by Christy Hicks on 6/3/20.
 //  Copyright © 2020 Knight Night. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
-
-
-@end
-
+import Foundation
---
+++ Astronomy-Objc/Astronomy-Objc/RoverDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoverDetailViewController.swift
+//  Astronomy-Objc
+//
+//  Created by Christy Hicks on 6/3/20.
+//  Copyright © 2020 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class RoverDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
